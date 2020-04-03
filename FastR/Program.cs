dfce9a4631fb5bf9de30d3d26e2642e8600c59cb
AlenX23 <alenx6123@gmail.com>
2020-04-03 11:43:35 +0300

    HI!

---
+++ FastR/Program.cs
@@ -0,0 +1,28 @@
+﻿using FastReport.Utils;
+using FastReport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FastR
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            Config.DesignerSettings.ShowInTaskbar = true;
+            Config.SplashScreenEnabled = true;
+
+            using (Report report = new Report())
+            {
+                report.Design();
+            }
+        }
+    }
+}
